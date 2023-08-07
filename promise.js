005962c9b8329bbdd780f42b218b9d02558793a9
aniket <yaniketever@gmail.com>
2023-08-07 18:38:51 +0530

    promise

--- promise.js
+++ promise.js
@@ -1,9 +1,24 @@
-const p = new Promise((resolve, reject) =>{
+// const p = new Promise((resolve, reject) =>{
     
-    reject(1);
+//     reject(1);
    
 
-})
+// })
 
-p.then((result) => console.log("output", result));
-p.catch((err) => console.log("error", err))
+// p.then((result) => console.log("output", result));
+// p.catch((err) => console.log("error", err))
+
+// What will be the output
+async function test(){
+    console.log('1');
+    await 10;
+
+    setTimeout(()=>{
+       console.log("Output");
+    })
+
+    console.log('1')
+
+}
+
+console.log(test());
